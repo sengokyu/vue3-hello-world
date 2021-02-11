063d1b752f7eabde2659d29415113ae29bbe6302
sengokyu <sengokyu+gh@gmail.com>
2021-02-12 00:17:23 +0900

    Install balm-ui

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "balm-ui/dist/balm-ui.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const BalmUI = require("balm-ui");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const BalmUIPlus = require("balm-ui/dist/balm-ui-plus");
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(BalmUI)
+  .use(BalmUIPlus)
+  .mount("#app");
