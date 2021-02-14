86393fd29d01836ea8acb37c83991a0aeb72cc89
sengokyu <sengokyu+gh@gmail.com>
2021-02-14 19:35:41 +0900

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <ui-top-app-bar type="1">Vue.js ことはじめ</ui-top-app-bar>
-  <div class="main-container">
-    <UiGallery></UiGallery>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import UiGallery from "./components/UiGallery.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    UiGallery,
-  },
-});
-</script>
 <style>
-.mdc-top-app-bar {
-  top: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-.main-container {
-  margin-top: 80px;
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import "balm-ui/dist/balm-ui.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/main.css";
+import router from "./router";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const BalmUI = require("balm-ui");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const BalmUIPlus = require("balm-ui/dist/balm-ui-plus");
 
 createApp(App)
+  .use(router)
   .use(BalmUI)
   .use(BalmUIPlus)
   .mount("#app");
