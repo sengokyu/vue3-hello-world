e86199475b4fe5379da349113b57060cf1945475
sengokyu <sengokyu+gh@gmail.com>
2021-02-15 22:05:28 +0900

    Add vuex

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/main.css";
 import router from "./router";
+import store from "./store";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const BalmUI = require("balm-ui");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const BalmUIPlus = require("balm-ui/dist/balm-ui-plus");
 
 createApp(App)
+  .use(store)
   .use(router)
   .use(BalmUI)
   .use(BalmUIPlus)
