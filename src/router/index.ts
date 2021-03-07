1d155669ed3cde79ae1c57d74808b4183b5ec65b
sengokyu <sengokyu+gh@gmail.com>
2021-03-07 23:33:22 +0900

    ルート設定しました。

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import Result from "../views/Result.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+  { path: "/", component: Home },
+  { path: "/result", component: Result },
 ];
 
 const router = createRouter({
